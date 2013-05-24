fab5221a8b928dc6e53e30ba669b4218a4d8aeb4
Emerson Wong <emerson@emersonw.com>
2013-05-24 03:29:10 -0700

    Makes the basic User model
    Includes has_secure_password for password hashing.

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+
+  before_save { self.email.downcase! }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: {minimum: 6 }
+  validates :password_confirmation, presence: true
+
+end
