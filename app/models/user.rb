8e947b496ed9d8f86d177f218d7c5081e6b8963a
Emerson Wong <emerson@emersonw.com>
2013-05-24 03:48:40 -0700

    Completes exercises for Chapter 6
    * Adds better e-mail regex (no repeating periods allowed)
    * Adds test to ensure any e-mail input for user is downcased

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   before_save { self.email.downcase! }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i 
 
   has_secure_password
 
