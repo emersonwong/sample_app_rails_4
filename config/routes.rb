31b92b50cabf45034d720361c9ec58be2c92b6d2
Emerson Wong <emerson@emersonw.com>
2013-05-25 00:55:18 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SampleApp::Application.routes.draw do
+
+  resources :users
+
   root to: 'static_pages#home'
+
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
