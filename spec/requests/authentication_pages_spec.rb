b176d68aa0dcfbce38d49337c53d9029c27dbf60
Emerson Wong <emerson@emersonw.com>
2013-05-30 22:13:21 -0700

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -73,6 +73,19 @@ describe "Authentication" do
 
           it { should have_title('Sign In') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+
+          it { should have_title('Sign In') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+
+          it { should have_title('Sign In') }
+        end
+
       end
 
       describe "when attempting to visit a protected page" do
@@ -128,6 +141,20 @@ describe "Authentication" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+      end
+
+      describe "in the Relationships controller" do
+
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }          
+        end
+
       end
 
     end
